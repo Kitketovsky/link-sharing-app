2b273b4028863837b2e575fd764ba517fa771825
Kitketovsky <kitketovsky@gmail.com>
2023-09-06 15:35:14 +0400

    Prop Navigation component

---
+++ src/lib/Navigation.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Link } from "svelte-routing";
+</script>
+
+<nav>
+  <Link to="/">Home</Link>
+  <Link to="/login">Login</Link>
+  <Link to="/profile">Profile</Link>
+  <Link to="/preview">Preview</Link>
+</nav>
+
+<style>
+  nav {
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    column-gap: 1.5rem;
+  }
+</style>
