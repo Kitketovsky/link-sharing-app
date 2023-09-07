8e320b691c842cfe57be4869c894f809aefc7aba
Kitketovsky <kitketovsky@gmail.com>
2023-09-07 18:58:19 +0400

    DevLink component

---
+++ src/lib/DevLink.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import Dropdown from "./Dropdown.svelte";
+  import Input from "./Input.svelte";
+  import LinkIcon from "./../assets/images/icon-link.svelte";
+  import type { IOptions } from "../conts/options";
+
+  export let number: number | null = null;
+  export let options: IOptions;
+  export let onRemoveLink: (id: string) => void;
+  export let link: { id: string; platform: string; url: string };
+
+  export let onSelectChange: (optionId: string) => void;
+</script>
+
+<div class="wrapper">
+  <div class="header">
+    <span>Link {number}</span>
+    <button
+      type="button"
+      class="body-m text-grey"
+      on:click={() => onRemoveLink(link.id)}>Remove</button
+    >
+  </div>
+  <Dropdown {options} bind:value={link.platform} {onSelectChange} />
+  <Input
+    type="url"
+    label="Link"
+    placeholder="e.g. https://service.com/yourname"
+    icon={LinkIcon}
+    value={link.url}
+    on:input
+  />
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.75rem;
+  }
+
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: var(--grey);
+
+    & span {
+      font-weight: 600;
+    }
+
+    & button {
+      border-radius: 0.5rem;
+      padding: 0.25rem;
+    }
+  }
+</style>
