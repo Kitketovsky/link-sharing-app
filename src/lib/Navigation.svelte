b6887047479de41426c3d0b98d85d2524fb550d6
Kitketovsky <kitketovsky@gmail.com>
2023-09-07 12:03:30 +0400

    Updated Navigation component

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,20 +1,63 @@
 <script lang="ts">
-  import { Link } from "svelte-routing";
+  import { navigate } from "svelte-routing";
+  import SmallLogoIcon from "./../assets/images/logo-devlinks-small.svg";
+  import LargeLogoIcon from "./../assets/images/logo-devlinks-large.svg";
+  import Button from "./Button.svelte";
+  import TabLink from "./TabLink.svelte";
+
+  import LinkIcon from "./../assets/images/icon-link.svelte";
+  import ProfileIcon from "./../assets/images/icon-profile-details-header.svelte";
+  import { pathname } from "../stores";
+
+  $: isPreviewPage = $pathname === "/preview";
 </script>
 
-<nav>
-  <Link to="/">Home</Link>
-  <Link to="/login">Login</Link>
-  <Link to="/profile">Profile</Link>
-  <Link to="/preview">Preview</Link>
-</nav>
+{#if isPreviewPage}
+  <nav>
+    <Button
+      label="Back to Editor"
+      mode="secondary"
+      on:click={() => navigate("/links")}
+    />
+    <Button label="Share Link" />
+  </nav>
+{:else}
+  <nav>
+    <div>
+      <img src={LargeLogoIcon} alt="Logo Icon" />
+    </div>
+
+    <div class="tabs">
+      <TabLink label="Links" to="/links">
+        <LinkIcon />
+      </TabLink>
+
+      <TabLink label="Profile Details" to="/profile">
+        <ProfileIcon />
+      </TabLink>
+    </div>
+
+    <Button
+      label="Preview"
+      mode="secondary"
+      on:click={() => navigate("/preview")}
+    />
+  </nav>
+{/if}
 
 <style>
   nav {
-    height: 60px;
+    margin: 1.5rem;
+    padding: 1rem 1rem 1rem 1.5rem;
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
     column-gap: 1.5rem;
+  }
+
+  .tabs {
+    display: flex;
+    align-items: center;
+    column-gap: 1rem;
   }
 </style>
