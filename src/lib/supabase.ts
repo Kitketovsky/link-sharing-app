acd6f51a03bcd11beab87c521967cb193b278be7
Kitketovsky <kitketovsky@gmail.com>
2023-09-10 17:04:33 +0400

    Supabase generated types

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "./../types/supabase";
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
@@ -7,6 +8,10 @@ if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   throw new Error("Supabase credentials hasn't been found!");
 }
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: { autoRefreshToken: true, detectSessionInUrl: true },
-});
+export const supabase = createClient<Database>(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
+  {
+    auth: { autoRefreshToken: true, detectSessionInUrl: true },
+  },
+);
